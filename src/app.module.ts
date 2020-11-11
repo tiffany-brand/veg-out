206e2381ee044034f7a35c1661df3c10c9d4a532
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-10 21:46:21 -0500

    database connected with Users entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    "type": "mysql",
+    "host": "localhost",
+    "port": 3306,
+    "username": "root",
+    "password": "root",
+    "database": "plant_power",
+    "entities": [User],
+    "synchronize": true
+  }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
+
+// The ormconfig.json file in root should connect to the TypeOrmModule 
+// which creates the database connection.  We need to turn "synchronize" to false before production.
---
+++ src/users/users.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './user.entity';
+
+
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
+export class UsersModule {}
