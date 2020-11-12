1f27004f16f0de4bec0ae50ff000ebb7b508b690
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-11 21:37:09 -0500

    basic user module complete

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,13 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    private readonly usersRepository: Repository<User>,
   ) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const user = new User();
+    user.firstName = createUserDto.firstName;
+    user.lastName = createUserDto.lastName;
+
+    return this.usersRepository.save(user);
+  }
 
   findAll(): Promise<User[]> {
     return this.usersRepository.find();
