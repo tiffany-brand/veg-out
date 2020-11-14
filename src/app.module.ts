64eccde84c82088afa5c0b772d770f95c7825721
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-14 13:37:40 -0500

    database now accepts process.env paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,28 @@ import { Connection } from 'typeorm';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 
+const logIt = () => {
+  console.log(process.env);
+}
+logIt();
+
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    "type": "mysql",
-    "host": "localhost",
-    "port": 3306,
-    "username": "root",
-    "password": "root",
-    "database": "plant_power",
-    "entities": [User],
-    "synchronize": true
+  imports: [
+  TypeOrmModule.forRoot({
+    type: "mysql",
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [User],
+    synchronize: true
   }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  constructor(private connection: Connection) {}
+  constructor(private connection: Connection) { }
 }
 
 // The ormconfig.json file in root should connect to the TypeOrmModule 
