55630a6ec625ef0bf1a5baa0eadf6d4073bb276d
Marc <mlanglois333@icloud.com>
2020-11-16 16:45:28 -0500

    Socket test component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,18 +5,21 @@ import Landing from './views/Auth0Test/Landing';
 import Auth0Test from './views/Auth0Test';
 import Wrapper from './components/Wrapper/Wrapper';
 import Keith from './views/KeithTest'
+import SocketTest from './views/Socket/SocketTest';
 
 function App() {
   return (
-    <div className="App">
+
       <Wrapper>
+
         <Switch>
           <Route path="/" exact component={Auth0Test} />
           <Route path="/landing" exact component={Landing} />
           <Route path="/keith-dev-test" exact component={Keith} />
+          <Route path="/socket" exact component={SocketTest} />
         </Switch>
       </Wrapper>
-    </div>
+    
   );
 }
 
---
+++ client/src/views/Socket/SocketTest.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function SocketTest(){
+
+    return (
+        <div>
+        <h1>socket test</h1>
+  <form>
+  <input type="text">
+    <br />
+    </input>
+    <button type="submit">send</button>
+  </form>
+  <div id="messages">
+
+  </div>
+</div>
+    );
+
+}
+export default SocketTest;
