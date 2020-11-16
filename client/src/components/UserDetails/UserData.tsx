37eb206688786fd34b15739e19195c835750cc80
Keith Tharp <keithstharp@gmail.com>
2020-11-16 14:08:55 -0500

    Home, Challenges, assc components built

---
+++ client/src/components/UserDetails/UserData.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './UserDetails.css';
+
+interface IUserData {
+  level: string;
+  character: string;
+}
+
+const userData: IUserData = {
+  level: "Forager",
+  character: "./assets/images/vegabunny-400.png"
+}
+
+export default function UserData() {
+
+  return (
+    <div className="user-details" >
+      <h3>• LEVEL •</h3>
+      <span className="user-level">{userData.level}</span>
+      <img className="userCharacter" src={userData.character} alt=""></img>
+    </div>
+  );
+}
--- client/src/views/Challenges/Challenges.tsx
+++ client/src/views/Challenges/Challenges.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
-import DetailCard from '../../components/DetailCard/DetailCard'
-import './Challenges.css'
+import DetailCard from '../../components/DetailCard/DetailCard';
+import UserData from '../../components/UserDetails/UserData';
+import './Challenges.css';
 
 export default function Challenges() {
 
-  const heading = "This is the heading";
-  const paragraph = "And the paragraph";
-
+  const pendingChallengesLI = ["Pending Challenge", "Another Pending", "Last Pender"];
+  const currentChallengesLI = ["Current Challenge", "Another Current", "Last Current"];
 
   return (
     <div className="challenges-container">
-      <DetailCard heading={heading} paragraph={paragraph} />
-      <DetailCard heading={heading} paragraph={paragraph} />
+      <div className="card-holder">
+        <h2>PENDING CHALLENGES</h2>
+        <DetailCard>
+          <ul>
+            {pendingChallengesLI.map(function (challenge, index) {
+              return <li key={index}>{challenge}</li>
+            })}
+          </ul>
+        </DetailCard>
+
+      </div>
+      <div className="user-data-holder">
+        <UserData />
+      </div>
+      <div className="card-holder">
+        <h2>CURRENT CHALLENGES</h2>
+        <DetailCard>
+          <ul>
+            {currentChallengesLI.map(function (challenge, index) {
+              return <li key={index}>{challenge}</li>
+            })}
+          </ul>
+        </DetailCard>
+      </div>
     </div>
   )
 
