fb58b6e3f06963f99cee1c237fb433a2345c696b
Marc <mlanglois333@icloud.com>
2020-11-18 13:34:43 -0500

    gameplay mechanics

---
+++ client/src/components/Gameplay/gameplay.tsx
@@ -0,0 +1,30 @@
+function Gameplay(p1: object, p2: object) {
+
+    const player1 = p1;
+    const player2 = p2;
+    let winner = undefined;
+
+    const p1attack = player1.currentattack / player2.currentattack;
+    const p2attack = player2.currentattack / player1.curentattack;
+
+    const p1turns = player2.currenthealth / p1attack;
+    const p2turns = player1.currenthealth / p2attack;
+
+    if (p1turns < p2turns) {
+        winner = player1.character_name;
+    }
+
+    else if (p2turns > p1turns) {
+        winner = player2.character_name;
+    }
+
+    else {
+        winner = "tie";
+    }
+
+    return winner;
+
+}
+
+
+export default Gameplay;
--- client/src/views/Socket/SocketTest.tsx
+++ client/src/views/Socket/SocketTest.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SocketComp from "../../components/Socket/SocketComp";
 
 
+
 function SocketTest(){
 
     return (
