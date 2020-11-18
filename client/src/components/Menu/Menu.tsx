4bcb6cb578bf064f2b749db760361b9503ca6ab0
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-11-18 12:04:46 -0500

    react dom

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ReactDOM from "react-router-dom";
 
 const useStyles = makeStyles({
   list: {
