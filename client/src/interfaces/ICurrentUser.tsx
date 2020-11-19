81c11da1b445af32c19f47b24653168252a27cfe
Tiffany Brand <brand2learn@gmail.com>
2020-11-18 21:22:32 -0500

    Update ICurrentUser interface

--- client/src/interfaces/ICurrentUser.tsx
+++ client/src/interfaces/ICurrentUser.tsx
@@ -1,8 +1,18 @@
 export default interface ICurrentUser {
+    _id?: string;
     email: string;
     auth0ID: string;
-    _id?: string;
-    firstName?: string;
-    lastName?: string;
     username?: string;
+    character_name?: string;
+    character_id?: string;
+    challenged: boolean;
+    currentChallenge: number;
+    currenthealth: number;
+    currentoffense: number;
+    currentdefense: number;
+    win: number;
+    loss: number;
+    tie: number;
+    level: number;
+
 }
