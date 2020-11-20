5a11aee097862396d9db4576a1801497fe99be10
Marc <mlanglois333@icloud.com>
2020-11-20 17:25:04 -0500

    use state

--- client/src/views/Challenges/Challenges.tsx
+++ client/src/views/Challenges/Challenges.tsx
@@ -3,83 +3,112 @@ import DetailCard from '../../components/DetailCard/DetailCard';
 import UserData from '../../components/UserData/UserData';
 import './Challenges.css';
 import challengesAPI from '../../utils/challengesAPI';
-import {useStoreContext} from "../../state/GlobalState";
-import {LOADING, SET_CHALLENGES} from "../../state/actions";
+import { useStoreContext } from "../../state/GlobalState";
+import { LOADING, SET_CHALLENGES } from "../../state/actions";
 
-export default function Challenges(){
-
-
-
-const[state, dispatch] = useStoreContext();
-
-
-let opponent: string="";
-
-console.log(state.challenges)
-
-
- function getChal(){
+export default function Challenges() {
 
 
 
-  if (state.challenges.date_started === "" && state.currentUser.currentChallenge!= undefined) {
+  const [state, dispatch] = useStoreContext();
 
-    let tempId = state.currentUser.currentChallenge;
-    let chalID = tempId.toString();
 
-    challengesAPI.getChallenge(chalID).then(res => {
+  let opponent: string = "";
 
-      dispatch({
-        type: SET_CHALLENGES,
-        challenges:{
-          ...state.challenges,
-          ...res
-        }
-      });
-      console.log(state.challenges)
-    }).catch(err => console.log(err));
+  console.log(state.challenges)
+
+
+  function getChal() {
+
+
+
+    if (state.challenges.date_started === "" && state.currentUser.currentChallenge != undefined) {
+
+      let tempId = state.currentUser.currentChallenge;
+      let chalID = tempId.toString();
+
+      challengesAPI.getChallenge(chalID).then(res => {
+
+        dispatch({
+          type: SET_CHALLENGES,
+          challenges: {
+            ...state.challenges,
+            ...res
+          }
+        });
+        console.log(state.challenges)
+      }).catch(err => console.log(err));
+    }
+    else { console.log("else fired" + state.currentUser) }
+
   }
-  else{console.log("else fired" + state.currentUser)}
 
-}
+  function getOpponent() {
+    if
+      (state.challenges.player_one._id == state.currentUser._id) {
+      return state.challenges.player_two.username;
+    }
+    else {
+      return state.challenges.player_one.username;
 
- function getOpponent(){
-      if 
-      (state.challenges.player_one._id == state.currentUser._id)
-      {opponent = state.challenges.player_two.username;
-       }
-      else{
-        opponent = state.challenges.player_one.username;
+    }
 
-}
+  }
+  if (state.currentUser.challenged === false) {
+    return (<div className="card-container">
+      <div className="card-holder">
+        <h2>PENDING CHALLENGES</h2>
+        <DetailCard>
+          <p>
+            No pending challenges
+    </p>
+        </DetailCard>
 
- }
-if (state.currentUser.challenged)
+      </div>
+      <div className="user-data-holder">
+        <UserData />
+      </div>
+      <div className="card-holder">
+        <h2>CURRENT CHALLENGES</h2>
+        <DetailCard>
+          <p>
+            No current challenges
+      
+          </p>
+        </DetailCard>
+      </div>
+    </div>)
+  }
 
-
-      return( <div className="card-container">
-          <div className="card-holder">
-            <h2>PENDING CHALLENGES</h2>
-            <DetailCard>
-              <p>
-                No pending challenges
-              </p>
-            </DetailCard>
+  else{
+    getChal();}
     
-          </div>
-          <div className="user-data-holder">
-            <UserData />
-          </div>
-          <div className="card-holder">
-            <h2>CURRENT CHALLENGES</h2>
-            <DetailCard>
-              <p>
-                Versus: {opponent}
-                Ends: {state.challenges.date_ending}
-              </p>
-            </DetailCard>
-          </div>
-        </div>)
-
   
+
+  return (<div className="card-container">
+    <div className="card-holder">
+      <h2>PENDING CHALLENGES</h2>
+      <DetailCard>
+        <p>
+          No pending challenges
+              </p>
+      </DetailCard>
+
+    </div>
+    <div className="user-data-holder">
+      <UserData />
+    </div>
+    <div className="card-holder">
+      <h2>CURRENT CHALLENGES</h2>
+      <DetailCard>
+        <p>
+          Versus: {getOpponent}
+          <br />
+                Ends: {state.challenges.date_ending}
+        </p>
+      </DetailCard>
+    </div>
+  </div>)
+
+
 };
