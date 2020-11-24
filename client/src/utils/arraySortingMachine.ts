faef52d37aa2fac14cd971990a985839b127fe24
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-24 16:47:06 -0500

    calculateMultiplierBonus function created

--- client/src/utils/arraySortingMachine.ts
+++ client/src/utils/arraySortingMachine.ts
@@ -1,3 +1,5 @@
+// this function takes 2 arrays, combines them and removes any duplicates
+
 export default {
     arraySort: (newArray: string[], databaseArray: string[]): string[] => {
         const sortedArray = newArray.concat(databaseArray.filter((item) => {
---
+++ client/src/utils/gameplayUtils/gamplayUtils.ts
@@ -0,0 +1,34 @@
+
+export default {
+    // This function will take in 1 users meallogs and calculate their multiplier bonus.
+    calculateMultiplierBonus: (arrayOfMeals: string[]): number => {
+        let bonus= 1;
+        const mealsOf3OrMore = [];
+        const mealsOf5OrMore = [];
+        const mealsOf7OrMore = [];
+        // This counts each meal entry and pushes it into the appropriate category according to meal length.
+        arrayOfMeals.forEach((meal) => {
+            console.log(`This is the meal: ${meal}`);
+            if(meal.length >= 7) {
+                mealsOf7OrMore.push(meal);
+            } else  if(meal.length >= 5){
+                mealsOf5OrMore.push(meal);
+            } else if(meal.length >= 3) {
+                mealsOf3OrMore.push(meal);
+            }
+        });
+        // This will assess the quantity of each diverse meal, delivering the highest bonus.
+        // This system is based on a week long challenge, for selecting challenge length we will need add length of challenge parameter.
+        if(mealsOf7OrMore.length >= 12) {
+            bonus = 7;
+        } else if (mealsOf5OrMore.length >= 12) {
+            bonus = 5;
+        } else if (mealsOf3OrMore.length >= 12) {
+            bonus = 3
+        } else {
+            bonus = 1;
+        }
+
+        return bonus;
+    }
+}
