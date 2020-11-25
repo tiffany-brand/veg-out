d2167b6863168adb0768ec57307cdd490b6cb827
Tiffany Brand <brand2learn@gmail.com>
2020-11-25 09:43:24 -0500

    Display total and unique veggies

--- client/src/components/ChallengeDisplay/ChallengeDisplay.tsx
+++ client/src/components/ChallengeDisplay/ChallengeDisplay.tsx
@@ -39,11 +39,14 @@ const ChallengeDisplay: React.FC<Props> = (props) => {
             <h2>{state.currentUser.nickname} Challenge Stats</h2>
             {props.position === 1 ? <p>Current Multiplier: {props.currentChallenge!.playerOne_currentMultiplier}</p> : <p>Current Multiplier: {props.currentChallenge!.playerTwo_currentMultiplier}</p>}
             { props.position === 1 ? <p>Current Score: {props.currentChallenge!.playerOne_currentScore}</p> : <p>Current Score: {props.currentChallenge!.playerTwo_currentScore}</p>}
+            { props.position === 1 ? <p>Total Veggies: {props.currentChallenge!.playerOne_totalVeggies.length}</p> : <p>Total Veggies: {props.currentChallenge!.playerTwo_totalVeggies.length}</p>}
+            { props.position === 1 ? <p>Total Unique Veggies: {props.currentChallenge!.playerOne_uniqueVeggies.length}</p> : <p>Total Unique Veggies: {props.currentChallenge!.playerTwo_uniqueVeggies.length}</p>}
 
             <h2>{props.currentChallenger!.nickname} Challenge Stats</h2>
             {props.position === 2 ? <p>Current Multiplier: {props.currentChallenge!.playerOne_currentMultiplier}</p> : <p>Current Multiplier: {props.currentChallenge!.playerTwo_currentMultiplier}</p>}
             { props.position === 2 ? <p>Current Score: {props.currentChallenge!.playerOne_currentScore} </p> : <p> Current Score: {props.currentChallenge!.playerTwo_currentScore}</p>}
-
+            { props.position === 2 ? <p>Total Veggies: {props.currentChallenge!.playerOne_totalVeggies.length}</p> : <p>Total Veggies: {props.currentChallenge!.playerTwo_totalVeggies.length}</p>}
+            { props.position === 2 ? <p>Total Unique Veggies: {props.currentChallenge!.playerOne_uniqueVeggies.length}</p> : <p>Total Unique Veggies: {props.currentChallenge!.playerTwo_uniqueVeggies.length}</p>}
 
             <Link to="/"> <Button variant="contained">Back to Login</Button> </Link>
         </div>
