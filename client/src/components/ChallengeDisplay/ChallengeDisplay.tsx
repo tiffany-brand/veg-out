75dc65e9b8e8428a07381b9464ef310687703f98
Tiffany Brand <brand2learn@gmail.com>
2020-11-25 09:45:49 -0500

    Reorder stats

--- client/src/components/ChallengeDisplay/ChallengeDisplay.tsx
+++ client/src/components/ChallengeDisplay/ChallengeDisplay.tsx
@@ -38,15 +38,18 @@ const ChallengeDisplay: React.FC<Props> = (props) => {
             <h2>{startDate.toFormat('LLL. dd yyyy')} - {endDate.toFormat('LLL. dd yyyy')}</h2>
             <h2>{state.currentUser.nickname} Challenge Stats</h2>
             {props.position === 1 ? <p>Current Multiplier: {props.currentChallenge!.playerOne_currentMultiplier}</p> : <p>Current Multiplier: {props.currentChallenge!.playerTwo_currentMultiplier}</p>}
-            { props.position === 1 ? <p>Current Score: {props.currentChallenge!.playerOne_currentScore}</p> : <p>Current Score: {props.currentChallenge!.playerTwo_currentScore}</p>}
+
             { props.position === 1 ? <p>Total Veggies: {props.currentChallenge!.playerOne_totalVeggies.length}</p> : <p>Total Veggies: {props.currentChallenge!.playerTwo_totalVeggies.length}</p>}
             { props.position === 1 ? <p>Total Unique Veggies: {props.currentChallenge!.playerOne_uniqueVeggies.length}</p> : <p>Total Unique Veggies: {props.currentChallenge!.playerTwo_uniqueVeggies.length}</p>}
+            { props.position === 1 ? <h3>Score: {props.currentChallenge!.playerOne_currentScore}</h3> : <h3>Score: {props.currentChallenge!.playerTwo_currentScore}</h3>}
 
             <h2>{props.currentChallenger!.nickname} Challenge Stats</h2>
             {props.position === 2 ? <p>Current Multiplier: {props.currentChallenge!.playerOne_currentMultiplier}</p> : <p>Current Multiplier: {props.currentChallenge!.playerTwo_currentMultiplier}</p>}
-            { props.position === 2 ? <p>Current Score: {props.currentChallenge!.playerOne_currentScore} </p> : <p> Current Score: {props.currentChallenge!.playerTwo_currentScore}</p>}
+
             { props.position === 2 ? <p>Total Veggies: {props.currentChallenge!.playerOne_totalVeggies.length}</p> : <p>Total Veggies: {props.currentChallenge!.playerTwo_totalVeggies.length}</p>}
             { props.position === 2 ? <p>Total Unique Veggies: {props.currentChallenge!.playerOne_uniqueVeggies.length}</p> : <p>Total Unique Veggies: {props.currentChallenge!.playerTwo_uniqueVeggies.length}</p>}
+            { props.position === 2 ? <h3>Score: {props.currentChallenge!.playerOne_currentScore} </h3> : <h3>Score: {props.currentChallenge!.playerTwo_currentScore}</h3>}
+
 
             <Link to="/"> <Button variant="contained">Back to Login</Button> </Link>
         </div>
