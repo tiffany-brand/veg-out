a5fa7c15595c9e729c5f4c4555091ce094719222
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-25 19:14:20 -0500

    moved the challenge stats

--- client/src/components/ChallengeStats/ChallengeStats.tsx
+++ client/src/components/ChallengeStats/ChallengeStats.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import DetailCard from '../../components/DetailCard/DetailCard';
+import DetailCard from '../DetailCard/DetailCard';
 
 
 import Grid from '@material-ui/core/Grid';
