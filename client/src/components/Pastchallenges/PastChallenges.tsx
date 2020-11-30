04f9368e0392b9da071c8b1cb4ca9e23fa33aa2e
Marc <mlanglois333@icloud.com>
2020-11-30 15:34:35 -0500

    stats table

--- client/src/components/Pastchallenges/PastChallenges.tsx
+++ client/src/components/Pastchallenges/PastChallenges.tsx
@@ -20,9 +20,9 @@ function PastChallenges() {
 
 
         challengesAPI.getChallenges().then((res) => {
-            
+
             res.data.forEach((item: { _id: any, playerOne: { _id: string; }; playerTwo: { _id: string; }; }) => {
-                if (item.playerOne._id == state.currentUser._id && state.currentUser.currentChallenge !== item._id|| item.playerTwo._id == state.currentUser._id && state.currentUser.currentChallenge !== item._id) {
+                if (item.playerOne._id == state.currentUser._id && state.currentUser.currentChallenge !== item._id || item.playerTwo._id == state.currentUser._id && state.currentUser.currentChallenge !== item._id) {
                     tempArray.push(item);
                 };
                 setPastArray(tempArray);
@@ -37,30 +37,39 @@ function PastChallenges() {
         )
     }
     else {
-        
+
         return (
             <div>
+                <h2>Current Record:</h2>
+                <br />
+                <p> Wins: {state.currentUser.wins}</p>
+                <br />
+                <p> Losses: {state.currentUser.losses}</p>
+                <br />
+                <p> Ties: {state.currentUser.ties}</p>
+                <br />
+                <h2>Past Challenge Details:</h2>
+                <br />
 
-            
 
-                {pastArray.map((item:any) => (                   
-                     <DetailCard children={ 
-                         <div>
-                        <h3>Ended On {item.dateEnding}</h3>
-                        <br />
-                        <p>Player One: {item.playerOne.nickname}
+                {pastArray.map((item: any) => (
+                    <DetailCard children={
+                        <div>
+                            <h3>Ended On {item.dateEnding}</h3>
                             <br />
+                            <p>Player One: {item.playerOne.nickname}
+                                <br />
             Score: {item.playerOne_currentScore}
-                            <br />
+                                <br />
 
             Player Two: {item.playerTwo.nickname}
-                            <br />
+                                <br />
 
             Score: {item.playerTwo_currentScore}
 
-                        </p>
+                            </p>
                         </div>}
-                 />))}
+                    />))}
 
 
             </div>
