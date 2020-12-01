2dabb6e04b47f89b7edb5b74cf38dd4c67db8701
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-30 20:46:38 -0500

    input ready, getting button prepped

--- client/src/views/AdminPage/AdminPage.tsx
+++ client/src/views/AdminPage/AdminPage.tsx
@@ -1,12 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import About from "../../components/About/About";
 import logo from '../../assets/images/stroked-vedgeIn-logo-1200.png';
 import Grid from '@material-ui/core/Grid';
+import VeggieAPI from '../../utils/veggiesAPI';
+import IVeggies from '../../interfaces/IVeggies';
 
 
 
 function AdminPage(): JSX.Element {
-    const newVeggieInput = useRef<HTMLInputElement | null>(null);
+    const [input, setInput] = useState("");
+
+    const updateSearchArray = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(event.currentTarget.value)  
+      };
+
+
+    const submitNewPlant = () => {
+        VeggieAPI.saveVeggie({plantName: input}).then((res) => {
+            console.log(res);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
 
   return (
 <>
@@ -16,8 +31,8 @@ function AdminPage(): JSX.Element {
         <div className="about-dark-box component-style">
         <h1>This is the admin page!  Don't tell anyone.</h1>
         <h3>Input your fruit or veggie below. Be certain of spelling before your submission!</h3>
-        <input ref={newVeggieInput} type="text" />
-          
+        <input value={input} onChange={updateSearchArray} type="text" />
+          <button type="button">Press To Submit</button>
         </div>
       </Grid>
     </div>
