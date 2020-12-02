2e0518bbb1e965d0d59dcb3ea3e50e5c7b339031
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-12-02 17:48:13 -0500

    finished confirmation of new added plant

--- client/src/views/AdminPage/AdminPage.tsx
+++ client/src/views/AdminPage/AdminPage.tsx
@@ -9,19 +9,22 @@ import IVeggies from '../../interfaces/IVeggies';
 
 function AdminPage(): JSX.Element {
     const [input, setInput] = useState("");
-    const [submissionResponse, setSubmissionResponse] = useState(null);
+    const [submissionResponse, setSubmissionResponse] = useState("");
     // This local state will record wether the most recent submission cleared or not. 
 
     const updateSearchArray = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(event.currentTarget.value)  
+        setInput(event.currentTarget.value);
+        setSubmissionResponse("");  
       };
 
 
     const submitNewPlant = () => {
         VeggieAPI.saveVeggie({plantName: input}).then((res) => {
             setInput("");
+            setSubmissionResponse("success");
             console.log(res);
         }).catch((err) => {
+          setSubmissionResponse("failure");
             console.log(err);
         })
     }
@@ -36,7 +39,10 @@ function AdminPage(): JSX.Element {
         <h3>Input your new fruit or veggie below. Be certain of spelling before your submission!</h3>
         <input value={input} onChange={updateSearchArray} type="text" />
           <button type="button" onClick={submitNewPlant}>Press To Submit Your New Plant.</button>
-        {/* Put a conditional block here that confirms if the submission worked or not. */}
+          {submissionResponse === "success" &&
+          <h3>Success!! Your veggie has been added to the DB.</h3>}
+          {submissionResponse === "failure" &&
+          <h3>Failure!! Are you sure it wasn't already in the DB? Hhhmm?</h3>}
         </div>
       </Grid>
     </div>
