3a06442ac58db6115e6e787962d32c47930cbcb8
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-12-06 21:33:00 -0500

    global state not updating nickname

--- client/src/components/UpdateUsername/UpdateUsername.tsx
+++ client/src/components/UpdateUsername/UpdateUsername.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
+import "./UpdateUsername.css";
+
 import { useStoreContext } from '../../state/GlobalState';
 import { SET_CURRENT_USER } from '../../state/actions';
 
 import userAPI from '../../utils/userAPI';
+import Alert from '@material-ui/lab/Alert';
 
 const UpdateUsername: React.FC = () => {
 
@@ -18,20 +21,26 @@ const UpdateUsername: React.FC = () => {
   }, []);
 
   const updateNickname = () => {
+    if (input.length > 5) {
       userAPI.saveUser({ ...state.currentUser, nickname: input}).then(res => {
           dispatch({
               type: SET_CURRENT_USER,
               currentUser: res.data
           })
       })
+      console.log(`updateNickname ${input}`);
+      console.log(`global state: ${state.currentUser.nickname}`)
+    } else {
+      console.log("failed");
+    }
   }
 
   return (
-    <>
+    <div className="delete-div">
       <label>Username</label>
-      <input onChange={event => setInput(event.target.value)} placeholder={state.currentUser.nickname}></input>
-      <button onClick={updateNickname}>Update</button>
-    </>
+    <input onChange={event => setInput(event.target.value)} placeholder={state.currentUser.nickname}></input>
+      <button className="green-button" onClick={updateNickname}>Update</button>
+    </div>
   );
 
 };
